8ceabc70818b3f19acd1428c49ee28b467dab44a
chuzksy-codeactive <chuzksy@gmail.com>
2020-09-29 10:57:28 +0100

    Add Customer DB Context, migration and update the database

--- Bank.API/Startup.cs
+++ Bank.API/Startup.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Bank.API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Bank.API
 {
@@ -24,6 +20,7 @@ namespace Bank.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BankContext>(options => options.UseSqlServer(Configuration["BankingDB"]));
             services.AddControllers();
         }
 
