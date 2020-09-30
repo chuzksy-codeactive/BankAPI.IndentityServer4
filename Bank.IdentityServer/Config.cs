fe339a55eb311d20671c04d0db9436732a777e3f
chuzksy-codeactive <chuzksy@gmail.com>
2020-09-30 19:23:29 +0100

    Configure IdentityServer and Requesting access token from IdentityServer using Postman

---
+++ Bank.IdentityServer/Config.cs
@@ -0,0 +1,27 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Bank.IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiScope> ApiScopes => new List<ApiScope>
+        {
+            new ApiScope("BankAPI", "Customer API for Bank")
+        };
+
+        public static IEnumerable<Client> Clients => new List<Client>
+        {
+            new Client
+            {
+                ClientId = "client",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+                AllowedScopes = { "BankAPI" }
+            }
+        };
+    }
+}
--- Bank.IdentityServer/Startup.cs
+++ Bank.IdentityServer/Startup.cs
@@ -16,6 +16,10 @@ namespace Bank.IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryClients(Config.Clients);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,7 +29,7 @@ namespace Bank.IdentityServer
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseIdentityServer();   
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
